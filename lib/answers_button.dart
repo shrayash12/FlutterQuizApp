20b36efa443e6bdf5f6d3eea44b392f82bc38c2b
shrayash12 <bhavayash@gmail.com>
2025-08-26 14:40:02 +0400

    Created Custome Widget Answers Button and added code for basic QuestionScreen Ui

---
+++ lib/answers_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, {super.key});
+
+  final String answerText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text(answerText));
+  }
+}
