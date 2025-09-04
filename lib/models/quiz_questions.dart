311bd2ae90efe5779a7b37e56949166352a707a4
shrayash12 <bhavayash@gmail.com>
2025-09-04 23:50:35 +0400

    mutating value in memory with shuffle

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
 class QuizQuestion {
   QuizQuestion(this.text, this.answers);
 
   final String text;
   final List<String> answers;
+
+  List<String> getSHuffleAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -19,18 +19,23 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            ...currentQuestion.getSHuffleAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
